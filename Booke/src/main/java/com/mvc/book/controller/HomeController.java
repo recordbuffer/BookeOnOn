31cf5dd0989d6730745719afe198bd0c1a76a7fa
msj-0709 <72193250+msj-0709@users.noreply.github.com>
2021-04-30 09:23:56 +0900

    ..

--- Booke/src/main/java/com/mvc/book/controller/HomeController.java
+++ Booke/src/main/java/com/mvc/book/controller/HomeController.java
@@ -32,13 +32,99 @@ public class HomeController {
 		return "home";
 	}
 	
+	//웰컴페이지로 이동
 	@RequestMapping("/welcome.do")
 	public String index() {
+		logger.info("WELCOME PAGE");
+		
 		return "welcome";
 	}
 	
+	
+	//메인 페이지로 이동
 	@RequestMapping("/main.do")
 	public String main() {
+		logger.info("MAIN PAGE");
+		
 		return "mainpage";
 	}
+	
+	//로그인 페이지로 이동
+	@RequestMapping("/loginform.do")
+	public String loginForm() {
+		logger.info("LOGIN FORM");
+		
+		return "loginpage";
+	}
+	
+	//회원가입 페이지로 이동
+	@RequestMapping("/signup.do")
+	public String signupform() {
+		logger.info("SIGN UP FORM");
+		
+		return "signuppage";
+	}
+	
+	//회원가입_아이디 중복 체크 페이지로 이동
+	@RequestMapping(value="/idChk.do")
+	public String signup_idChk() {
+		logger.info("SIGNUP ID CHECK POP UP");
+		
+		return "signuppage_idchk";
+	}
+	
+	//회원 정보 수정 페이지로 이동
+	@RequestMapping("/updateform.do")
+	public String updateform() {
+		logger.info("UPDATE MEMBER INFO PAGE");
+	
+		return "updatepage";
+	}
+	
+	//회원 탈퇴 페이지로 이동
+	@RequestMapping("/md_aks.do")
+	public String mdelete() {
+		logger.info("MEMBER DELETE PAGE");
+		
+		return "mdeletepage";
+	}
+	
+	//회원 탈퇴 확인 페이지로 이동
+	@RequestMapping(value="/deleteChk.do")
+	public String mdelete_Chk() {
+		logger.info("MEMBER DELETE CHECK POP UP");
+		
+		return "mdeletepage_Chk";
+	}
+	
+	//친구 검색 페이지로 이동
+		@RequestMapping("/friend.do")
+		public String friend() {
+			logger.info("FRIEND PAGE");
+			
+			return "friendpage";
+		}
+		
+		
+	//친구 검색 페이지로 이동
+	@RequestMapping("/fsearch.do")
+	public String fsearch() {
+		logger.info("FRIEND SEARCH PAGE");
+		
+		return "fsearchpage";
+	}
+	
+	
+	//친구 검색 페이지 > 검색 결과 페이지로 이동
+	@RequestMapping("/fres.do")
+	public String fres() {
+		logger.info("FRIEND SEARCH RES PAGE");
+		
+		return "fsearchpage_res";
+	}
+	
+	
+	
+	
+	
 }
