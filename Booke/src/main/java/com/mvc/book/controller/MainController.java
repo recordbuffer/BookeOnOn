f9bbae450b45251103b4c2d4ca78c2df85fc718c
owo14 <owo_14@daum.net>
2021-04-30 14:48:18 +0900

    11

--- Booke/src/main/java/com/mvc/book/controller/MainController.java
+++ Booke/src/main/java/com/mvc/book/controller/MainController.java
@@ -14,189 +14,189 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+   private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String home(Locale locale, Model model) {
+      logger.info("Welcome home! The client locale is {}.", locale);
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+      Date date = new Date();
+      DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
-		String formattedDate = dateFormat.format(date);
+      String formattedDate = dateFormat.format(date);
 
-		model.addAttribute("serverTime", formattedDate);
+      model.addAttribute("serverTime", formattedDate);
 
-		return "home";
-	}
+      return "home";
+   }
 
-	// [시작 > 메인]
-	// 웰컴페이지로 이동
-	@RequestMapping("/welcome.do")
-	public String index() {
-		logger.info("WELCOME PAGE");
+   // [시작 > 메인]
+   // 웰컴페이지로 이동
+   @RequestMapping("/welcome.do")
+   public String index() {
+      logger.info("WELCOME PAGE");
 
-		return "welcome";
-	}
+      return "welcome";
+   }
 
-	// 메인 페이지로 이동
-	@RequestMapping("/main.do")
-	public String main() {
-		logger.info("MAIN PAGE");
+   // 메인 페이지로 이동
+   @RequestMapping("/main.do")
+   public String main() {
+      logger.info("MAIN PAGE");
 
-		return "mainpage";
-	}
+      return "mainpage";
+   }
 
-	// [회원 로그인]
-	// 로그인 페이지로 이동
-	@RequestMapping("/loginform.do")
-	public String loginForm() {
-		logger.info("LOGIN FORM");
+   // [회원 로그인]
+   // 로그인 페이지로 이동
+   @RequestMapping("/loginform.do")
+   public String loginForm() {
+      logger.info("LOGIN FORM");
 
-		return "loginpage";
-	}
+      return "loginpage";
+   }
 
-	// 회원가입 페이지로 이동
-	@RequestMapping("/signup.do")
-	public String signupform() {
-		logger.info("SIGN UP FORM");
+   // 회원가입 페이지로 이동
+   @RequestMapping("/signup.do")
+   public String signupform() {
+      logger.info("SIGN UP FORM");
 
-		return "signuppage";
-	}
+      return "signuppage";
+   }
 
-	// 회원가입_아이디 중복 체크 페이지로 이동
-	@RequestMapping(value = "/idChk.do")
-	public String signup_idChk() {
-		logger.info("SIGNUP ID CHECK POP UP");
+   // 회원가입_아이디 중복 체크 페이지로 이동
+   @RequestMapping(value = "/idChk.do")
+   public String signup_idChk() {
+      logger.info("SIGNUP ID CHECK POP UP");
 
-		return "signuppage_idchk";
-	}
+      return "signuppage_idchk";
+   }
 
-	// [메인 페이지]
-	// 메인_지도 위치 팝업
-	@RequestMapping("/detailmap.do")
-	public String detailmap() {
-		return "detailmap";
-	}
+   // [메인 페이지]
+   // 메인_지도 위치 팝업
+   @RequestMapping("/detailmap.do")
+   public String detailmap() {
+      return "detailmap";
+   }
 
-	// 책 소개 페이지로 이동
-	@RequestMapping("/bookintro.do")
-	public String bookintro() {
-		return "bookintro";
-	}
+   // 책 소개 페이지로 이동
+   @RequestMapping("/bookintro.do")
+   public String bookintro() {
+      return "bookintro";
+   }
 
-	// 책 소개 페이지_팝업 상세 정보
-	@RequestMapping("/detailpopup.do")
-	public String detailpopup() {
-		return "detailpopup";
-	}
-	
-	
-	// [ 친구 검색 ]
-	// 친구 검색 페이지로 이동
-	@RequestMapping("/fsearch.do")
-	public String fsearch() {
-		logger.info("FRIEND SEARCH PAGE");
+   // 책 소개 페이지_팝업 상세 정보
+   @RequestMapping("/detailpopup.do")
+   public String detailpopup() {
+      return "detailpopup";
+   }
+   
+   
+   // [ 친구 검색 ]
+   // 친구 검색 페이지로 이동
+   @RequestMapping("/fsearch.do")
+   public String fsearch() {
+      logger.info("FRIEND SEARCH PAGE");
 
-		return "fsearchpage";
-	}
+      return "fsearchpage";
+   }
 
-	// 친구 검색 페이지 > 검색 결과 페이지로 이동
-	@RequestMapping("/fres.do")
-	public String fres() {
-		logger.info("FRIEND SEARCH RES PAGE");
+   // 친구 검색 페이지 > 검색 결과 페이지로 이동
+   @RequestMapping("/fres.do")
+   public String fres() {
+      logger.info("FRIEND SEARCH RES PAGE");
 
-		return "fsearchpage_res";
-	}
+      return "fsearchpage_res";
+   }
 
-	
-	//[ 쪽지 기능]
-	// 쪽지 보내기 
-	@RequestMapping("/msg.do")
-	public String msg() {
-		return "msg";
-	}
-	
-	
-	// [ 책 검색 ]
-	// 책 검색 페이지로 이동
-	@RequestMapping("/bsearch.do")
-	public String bsearch() {
-		return "bsearchpage";
-	}
+   
+   //[ 쪽지 기능]
+   // 쪽지 보내기 
+   @RequestMapping("/msg.do")
+   public String msg() {
+      return "msg";
+   }
+   
+   
+   // [ 책 검색 ]
+   // 책 검색 페이지로 이동
+   @RequestMapping("/bsearch.do")
+   public String bsearch() {
+      return "bsearchpage";
+   }
 
-	// 책 검색 결과 페이지로 이동
-	@RequestMapping("/bres.do")
-	public String bres() {
-		return "bsearchpage_res";
-	}
+   // 책 검색 결과 페이지로 이동
+   @RequestMapping("/bres.do")
+   public String bres() {
+      return "bsearchpage_res";
+   }
 
-	// [ 서재 ]
-	// 서재 페이지로 이동
-	@RequestMapping("/bcase.do")
-	public String bcase() {
-		return "bookcase";
-	}
+   // [ 서재 ]
+   // 서재 페이지로 이동
+   @RequestMapping("/bcase.do")
+   public String bcase() {
+      return "bookcase";
+   }
 
-	// 서재 페이지_캘린더로 이동
-	@RequestMapping("/bcaseCal.do")
-	public String bcaseCal() {
-		return "bookcase_cal";
-	}
+   // 서재 페이지_캘린더로 이동
+   @RequestMapping("/bcaseCal.do")
+   public String bcaseCal() {
+      return "bookcase_cal";
+   }
 
-	// [설정]
-	// 설정 페이지로 이동
-	@RequestMapping("/setting.do")
-	public String setting() {
-		return "setting";
-	}
+   // [설정]
+   // 설정 페이지로 이동
+   @RequestMapping("/setting.do")
+   public String setting() {
+      return "setting";
+   }
 
-	// 회원 정보 수정 페이지로 이동
-	@RequestMapping("/updateform.do")
-	public String updateform() {
-		logger.info("UPDATE MEMBER INFO PAGE");
+   // 회원 정보 수정 페이지로 이동
+   @RequestMapping("/updateform.do")
+   public String updateform() {
+      logger.info("UPDATE MEMBER INFO PAGE");
 
-		return "updatepage";
-	}
+      return "updatepage";
+   }
 
-	// 회원 탈퇴 페이지로 이동
-	@RequestMapping("/md_aks.do")
-	public String mdelete() {
-		logger.info("MEMBER DELETE PAGE");
+   // 회원 탈퇴 페이지로 이동
+   @RequestMapping("/md_aks.do")
+   public String mdelete() {
+      logger.info("MEMBER DELETE PAGE");
 
-		return "mdeletepage";
-	}
+      return "mdeletepage";
+   }
 
-	// 회원 탈퇴 확인 페이지로 이동
-	@RequestMapping(value = "/deleteChk.do")
-	public String mdelete_Chk() {
-		logger.info("MEMBER DELETE CHECK POP UP");
+   // 회원 탈퇴 확인 페이지로 이동
+   @RequestMapping(value = "/deleteChk.do")
+   public String mdelete_Chk() {
+      logger.info("MEMBER DELETE CHECK POP UP");
 
-		return "mdeletepage_Chk";
-	}
+      return "mdeletepage_Chk";
+   }
 
-	// 친구 관리 페이지로 이동
-	@RequestMapping("/friend.do")
-	public String friend() {
-		logger.info("FRIEND PAGE");
+   // 친구 관리 페이지로 이동
+   @RequestMapping("/friend.do")
+   public String friend() {
+      logger.info("FRIEND PAGE");
 
-		return "friendpage";
-	}
+      return "friendpage";
+   }
 
-	// 쪽지 관리 페이지로 이동
+   // 쪽지 관리 페이지로 이동
 
-	
-	//[ 커뮤니티 ]
-	//커뮤니티 페이지로 이동
-	@RequestMapping("/communityMain.do")
-	public String communityMain() {
-		return "communityMain";
-	}
-	
-	//커뮤니티 채팅방으로 이동
-	@RequestMapping("/communityChat.do")
-	public String communityChat() {
-		return "communityChat";
-	}
+   
+   //[ 커뮤니티 ]
+   //커뮤니티 페이지로 이동
+   @RequestMapping("/communityMain.do")
+   public String communityMain() {
+      return "communityMain";
+   }
+   
+   //커뮤니티 채팅방으로 이동
+   @RequestMapping("/communityChat.do")
+   public String communityChat() {
+      return "communityChat";
+   }
 
 }
